1e7c0a4a35acbeaf7f6201d27ee8c3cca5c1fdb2
Sherrod Blankner <mooncalf@Sherrods-MacBook-Pro.local>
2016-09-18 17:42:07 -0700

    cursor in wrong position in rstudio. very frustrating

--- plot4.R
+++ plot4.R
@@ -34,7 +34,7 @@ plot(feb$DateTime,feb$Voltage, type="l", xlab="datetime", ylab="Voltage")
 #view it on screen
 #overlay plots and set y axis
 plot(feb$DateTime,feb$Sub_metering_1, type="l", col="black", ylim=c(0,38), xlab="", ylab="Energy Sub metering")
-legend("topright", c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), bty="n",lwd=c(2.5,2.5, bty="n", 2.5),col=c("black","red","blue"), cex=1) 
+legend("topright", c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), bty="n",lwd=c(2.5,2.5,2.5),col=c("black","red","blue"), cex=1) 
 par(new=TRUE)
 plot(feb$DateTime,feb$Sub_metering_2, type="l", col="red",  ylim=c(0,38), xlab="", ylab="")
 par(new=TRUE)
@@ -70,7 +70,7 @@ plot(feb$DateTime,feb$Voltage, type="l", xlab="datetime", ylab="Voltage")
 #view it on screen
 #overlay plots and set y axis
 plot(feb$DateTime,feb$Sub_metering_1, type="l", col="black", ylim=c(0,38), xlab="", ylab="Energy Sub metering")
-legend("topright", c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), bty="n", lwd=c(2.5,2.5,bty="n", 2.5),col=c("black","red","blue"), cex=1) 
+legend("topright", c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), bty="n", lwd=c(2.5,2.5, 2.5),col=c("black","red","blue"), cex=1) 
 par(new=TRUE)
 plot(feb$DateTime,feb$Sub_metering_2, type="l", col="red",  ylim=c(0,38), xlab="", ylab="")
 par(new=TRUE)
